b97f7bc8d5c7d8f032a3d72fbae2485408d0b702
Raiden Inc <mikepetrillo@gmail.com>
2018-12-17 21:52:42 -0500

    Testing time delay

--- nidus/main.py
+++ nidus/main.py
@@ -9,19 +9,20 @@ import time
 
 def main():
 
-    b = Board(dims = (4, 4), players = 4)
+    b = Board(dims = (4, 4), players = 2)
     b.new_game()
 
     while b.winner is None:
 
         s = b.__state__()
         print(str(s))
-
+time.sleep(2)
         b.new_turn()
 
     s = b.__state__()
     print(str(s))
-    print('{} is the winner.'.format(b.winner))
+time.sleep(2)
+    print(b.winner)
 
     return
 if __name__ == '__main__':
